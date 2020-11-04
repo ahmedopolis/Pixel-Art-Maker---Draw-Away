85d2376570748a49f85515c776082de07b2807f8
aimogue <ahmed.ibrahimmogue@gmail.com>
2020-11-04 04:32:36 -0500

    🔖 Feat: Cells change color on click

--- designs.js
+++ designs.js
@@ -1,82 +1,68 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-// Your code goes here!
-    var submitForm = document.getElementById('sizePicker');
-    submitForm.addEventListener('submit', buildGrid);
+  const colorPickerElement = document.getElementById("colorPicker");
+  var colorPickerElementValue = colorPickerElement.value;
+  colorPickerElement.addEventListener("input", function () {
+    colorPickerElementValue = colorPickerElement.value;
+  });
 
-    function buildGrid(event) {
-        event.preventDefault();
-    
-        var tableElement = document.getElementById('pixelCanvas');
-        while (tableElement.hasChildNodes()) {
-            tableElement.removeChild(tableElement.lastChild);
-        }
-    
-        var tableHeightValue = document.getElementById('inputHeight').value;
-        var tableWidthValue = document.getElementById('inputWidth').value;
-        const fullGridDiv = document.createElement('div');
-    
-        for (var i = 1; i <= tableHeightValue; i++) {
-            const tableRowInstance = document.createElement('tr');
-            for (var j = 1; j <= tableWidthValue; j++) {
-                const tableCellInstance = document.createElement('td');
-                tableCellInstance.style.background = "white";
-                tableRowInstance.appendChild(tableCellInstance);
-            }
-            fullGridDiv.appendChild(tableRowInstance);
-        }
-    
-        tableElement.appendChild(fullGridDiv);
+  const submitForm = document.getElementById("sizePicker");
+  submitForm.addEventListener("submit", buildGrid);
+
+  function buildGrid(event) {
+    event.preventDefault();
+    var tableElement = document.getElementById("pixelCanvas");
+    while (tableElement.hasChildNodes()) {
+      tableElement.removeChild(tableElement.lastChild);
     }
-
-    var colorInputValue = document.getElementById('colorPicker').value;    
+    var tableHeightValue = document.getElementById("inputHeight").value;
+    var tableWidthValue = document.getElementById("inputWidth").value;
+    const fullGridDiv = document.createElement("div");
+    for (var i = 1; i <= tableHeightValue; i++) {
+      const tableRowInstance = document.createElement("tr");
+      for (var j = 1; j <= tableWidthValue; j++) {
+        const tableCellInstance = document.createElement("td");
+        tableCellInstance.style.backgroundColor = "white";
+        tableCellInstance.onclick = function () {
+          tableCellInstance.style.backgroundColor = colorPickerElementValue;
+        };
+        tableRowInstance.appendChild(tableCellInstance);
+      }
+      fullGridDiv.appendChild(tableRowInstance);
+    }
+    tableElement.appendChild(fullGridDiv);
+  }
 }
 
-
-
-
-/**
- * Additional Code Starting Here.
- */
-
-function bodyEdits() {
-    document.body.style.backgroundImage = "url('pattern-background.jpg')";
+function backgroundImageEdit() {
+  document.body.style.backgroundImage = "url('pattern-background.jpg')";
 }
 
-function mainHeadingEdits() {
-    window.addEventListener('load', loadRainbowMainHeadingText);
-}
-
-function loadRainbowMainHeadingText() {
-    var instanceOfHeadingList = document.getElementsByTagName('h1');
+function mainHeadingRandomColorEdit() {
+  window.addEventListener("load", loadRainbowMainHeadingText);
+  function loadRainbowMainHeadingText() {
+    var instanceOfHeadingList = document.getElementsByTagName("h1");
     generateRandomRainbowMainHeading(instanceOfHeadingList[0]);
-}
-
-function generateRandomRainbowMainHeading(element) {
+  }
+  function generateRandomRainbowMainHeading(element) {
     var mainHeadingText = element.innerText;
     element.innerHTML = "";
     for (let i = 0; i < mainHeadingText.length; i++) {
-        let charMainHeadingText = document.createElement("span");
-        charMainHeadingText.style.color = randomColorString();
-        charMainHeadingText.innerHTML = mainHeadingText[i];
-        element.appendChild(charMainHeadingText);
+      let charMainHeadingText = document.createElement("span");
+      charMainHeadingText.style.color = randomColorString();
+      charMainHeadingText.innerHTML = mainHeadingText[i];
+      element.appendChild(charMainHeadingText);
     }
-
-}
-
-function randomColorString() {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  }
+  function randomColorString() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return "#" + randomColor;
+  }
 }
 
-function additionnalEdits() {
-    bodyEdits();
-    mainHeadingEdits();
+function start() {
+  backgroundImageEdit();
+  mainHeadingRandomColorEdit();
+  makeGrid();
 }
 
-additionnalEdits();
-makeGrid();
+start();
